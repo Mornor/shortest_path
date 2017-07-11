f700b46f497390e2d46dace3408d41d2392cbd02
Celien Nanson <cesliens@gmail.com>
2017-07-11 21:23:10 +0200

    Set up different files for Udacity and my solution

--- shortest_path.py
+++ shortest_path.py
@@ -35,47 +35,6 @@ def check_item(current_path_array, checked_grid):
 	for path in current_path_array:
 		checked_grid[path[1]][path[2]] = -1
 	return checked_grid 
-
-def search_udacity(grid, init, goal, cost): 
-	"""
-	Method implemented by Udacity
-	"""
-	closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
-	closed[init[0]][init[1]] = 1
-
-	x = init[0]
-	y = init[1]
-	g = 0
-
-	open = [[g, x, y]]
-
-	found = False  # flag that is set when search is complete
-	resign = False # flag set if we can't find expand
-
-	while not found and not resign:
-		if len(open) == 0:
-			resign = True
-		else:
-			open.sort()
-			open.reverse()
-			next = open.pop()
-			x = next[1]
-			y = next[2]
-			g = next[0]
-			
-			if x == goal[0] and y == goal[1]:
-				found = True
-			else:
-				for i in range(len(delta)):
-					x2 = x + delta[i][0]
-					y2 = y + delta[i][1]
-					if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
-						if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-							g2 = g + cost
-							open.append([g2, x2, y2])
-							closed[x2][y2] = 1
-							print(open)
-	return open
 	
 def search(grid, init, goal, cost):
 	# Init values
@@ -90,7 +49,6 @@ def search(grid, init, goal, cost):
 
 	return path_array
 
-#print(search(grid, init, goal, cost))
-result_udacity = search_udacity(grid, init, goal, cost)
-print(result_udacity)
+result = search(grid, init, goal, cost)
+#print(result)
 
