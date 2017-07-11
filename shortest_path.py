0ff53dcaec4cfb175be8582f2abf424ac86f4cd0
Celien Nanson <cesliens@gmail.com>
2017-07-11 20:05:10 +0200

    Add code from Udacity

--- shortest_path.py
+++ shortest_path.py
@@ -16,18 +16,18 @@
 #   1 = Occupied space
 
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1, 0]]
+		[0, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 1, 0],
+		[0, 0, 1, 1, 1, 0],
+		[0, 0, 0, 0, 1, 0]]
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1 # Cost of each mouvement
 
 delta = [[-1, 0], # go up
-         [ 0,-1], # go left
-         [ 1, 0], # go down
-         [ 0, 1]] # go right
+		 [ 0,-1], # go left
+		 [ 1, 0], # go down
+		 [ 0, 1]] # go right
 
 delta_name = ['^', '<', 'v', '>']
 
@@ -36,22 +36,61 @@ def check_item(current_path_array, checked_grid):
 		checked_grid[path[1]][path[2]] = -1
 	return checked_grid 
 
-#def expands(current_path, checked_grid): 
+def search_udacity(grid, init, goal, cost): 
+	"""
+	Method implemented by Udacity
+	"""
+	closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+	closed[init[0]][init[1]] = 1
 
+	x = init[0]
+	y = init[1]
+	g = 0
 
+	open = [[g, x, y]]
+
+	found = False  # flag that is set when search is complete
+	resign = False # flag set if we can't find expand
+
+	while not found and not resign:
+		if len(open) == 0:
+			resign = True
+		else:
+			open.sort()
+			open.reverse()
+			next = open.pop()
+			x = next[1]
+			y = next[2]
+			g = next[0]
+			
+			if x == goal[0] and y == goal[1]:
+				found = True
+			else:
+				for i in range(len(delta)):
+					x2 = x + delta[i][0]
+					y2 = y + delta[i][1]
+					if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+						if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+							g2 = g + cost
+							open.append([g2, x2, y2])
+							closed[x2][y2] = 1
+							print(open)
+	return open
+	
 def search(grid, init, goal, cost):
-    # Init values
-    g_value = 0
-    pos_x = init[0]
-    pos_y = init[1]
-    path_array = [[g_value, pos_x, pos_y]]
-    checked_grid = grid
+	# Init values
+	g_value = 0
+	pos_x = init[0]
+	pos_y = init[1]
+	path_array = [[g_value, pos_x, pos_y]]
+	checked_grid = grid
+	
+	# Check the already seen positions
+	#checked_grid = check_item(path_array, checked_grid)
 
-    # Check the already seen positions
-    checked_grid = check_item(path_array, checked_grid)    
-    print(checked_grid)
-
-    return path_array
+	return path_array
 
 #print(search(grid, init, goal, cost))
-search(grid, init, goal, cost)
+result_udacity = search_udacity(grid, init, goal, cost)
+print(result_udacity)
+
